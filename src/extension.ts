a19a88484e5c851c1558a6321217fdab404b412a
Volkan Ozdamar <ozdamar.volkan@gmail.com>
2020-01-11 23:00:41 +0300

    New Features

--- src/extension.ts
+++ src/extension.ts
@@ -18,24 +18,20 @@ export async function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', async () => {
-		async function getUserInfo() {
-			let userInputWindow =   vscode.window.showInputBox({value: '192.0.0.1',placeHolder: 'Select where do you want to run your test?'});
-			return userInputWindow;
-		}
-		
-		async function runCmd(runnerType : string | undefined,locustPath: string,fileLocation: string,platform: string | undefined){
-			if(runnerType === "local"){
-				terminal.sendText(locustPath+' -f '+fileLocation+' --host='+platform);
-			}
-			if(runnerType === "master"){
-				terminal.sendText(locustPath+' -f '+fileLocation+' --host='+platform+' --master');
-			}
-			if(runnerType === "slave"){
-				getUserInfo();
-			}
-	    }
-		// The code you place here will be executed every time your command is executed
+	// The code you place here will be executed every time your command is executed
+	function wait(ms: number){
+		var start = new Date().getTime();
+		var end = start;
+		while(end < start + ms) {
+		  end = new Date().getTime();
+	   }
+	 }
+
+
+
+
+
+	let disposable = vscode.commands.registerCommand('extension.ozdamar', async () => {
 		const locustPath = '/usr/local/bin/locust'
 		// Display a message box to the user
 		var testFolder = vscode.workspace.rootPath+'/tests';
@@ -47,24 +43,29 @@ export async function activate(context: vscode.ExtensionContext) {
           }); 
 
 		let runnerType = await vscode.window.showQuickPick(['local', 'master','slave']);
-		const testfile = await vscode.window.showQuickPick(testfiles);
-		// Test File Location
-		const fileLocation =  'tests/'+testfile
-
 		const platform = await vscode.window.showInputBox({value: 'stage',placeHolder: 'Select where do you want to run your test?'});
-		//await vscode.window.showInformationMessage('Your Test is Running '+input);
-		const terminal = (<any>vscode.window).createTerminal({ name: `Locust Runner `+platform});
-		
-	//	const cterminal = (<any>vscode.window).createTerminal({ name: `Locust Chrome `+platform});
-		runCmd(runnerType,locustPath,fileLocation,platform)
-		await terminal.show();
-
-		
-
+		const terminal = await (<any>vscode.window).createTerminal({ name: `Locust Runner `+platform});
+		const cterminal = (<any>vscode.window).createTerminal({ name: `Locust Chrome `+platform});
+		const testfile = await vscode.window.showQuickPick(testfiles);
+		const fileLocation =  'tests/'+testfile;
+		if(runnerType === "local"){
+			await terminal.sendText(locustPath+' -f '+fileLocation+' --host='+platform);
+			await terminal.show();
+		}
+		if(runnerType === "master"){
+			await terminal.sendText(locustPath+' -f '+fileLocation+' --host='+platform+' --master');
+			await terminal.show();
+		}
 	
-	//	await cterminal.sendText('/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome "http://localhost:8089"');
-
-		
+		if(runnerType === "slave"){
+			let hostIp = await  vscode.window.showInputBox({value: '192.0.0.1',placeHolder: 'Write your [MASTER] machine IP'});
+			let port = await  vscode.window.showInputBox({value: '80',placeHolder: 'Select where do you want to run your test?'});
+			await terminal.sendText(locustPath+' -f '+' --host='+platform+' --master-host='+hostIp+' --master-port='+port);
+			await terminal.show();
+		}
+		wait(4000)
+		await cterminal.sendText('/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome "http://localhost:8089"');
+		await vscode.window.showInformationMessage('Chrome Browser is UP');
 	});
 
 	context.subscriptions.push(disposable);
